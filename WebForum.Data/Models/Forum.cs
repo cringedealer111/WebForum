9d8c3768fd39b51b641ef33c5a149e0db01c45e2
Maksim Zolotarev <maksimka-zolotarev-2016@mail.ru>
2023-10-21 02:56:54 +0300

    Добавьте файлы проекта.

---
+++ WebForum.Data/Models/Forum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebForum.Data.Models
+{
+    public class Forum
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public string ImageUrl { get; set; }
+        public ApplicationUser User { get; set; }
+        public IEnumerable<Post> Posts { get; set; }
+    }
+}
---
+++ WebForum.Data/Models/PostReply.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebForum.Data.Models
+{
+    public class PostReply
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public DateTime Created { get; set; }
+        public ApplicationUser User { get; set; }
+    }
+}
